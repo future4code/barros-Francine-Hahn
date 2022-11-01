a98e8128c4b1cdeb34e0eb8ffb41e55611d1f448
Francine Hahn <fran_hahn@hotmail.com>
2022-11-01 18:00:49 -0300

    Exercícios de 1 a 12

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,10 @@
+import { Product } from "./types"
+
+export const products: Product[] = [
+    {id: 'asdfgh1', name: 'Vestido', price: 129.90},
+    {id: 'asdfgh2', name: 'Blusa', price: 59.90},
+    {id: 'asdfgh3', name: 'Saia', price: 79.90},
+    {id: 'asdfgh4', name: 'Sapato', price: 259.90},
+    {id: 'asdfgh5', name: 'Calça jeans', price: 150.90},
+    {id: 'asdfgh6', name: 'Casaco', price: 310.90}
+]
